3eebe0f7ce8e6dcf9153bd14ff4ed40a94ba9014
Michael Sekol <mjsekol@live.com>
2020-09-03 14:17:42 -0400

    Added Classes

---
+++ CSharpBasics/CharacterClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpBasics
+{
+    class CharacterClass
+    {
+        public string CharacterName; 
+        public int CharacterLevel; 
+        public string CharacterRace; 
+        public double HitPoints; 
+        public string CharClass;
+        public bool InGuild;
+
+        public void CharacterSelect()
+        {
+            if (CharacterLevel >= 100)
+            {
+                Console.WriteLine($"Welcome {CharacterName}. You are mighty {CharClass}, of the {CharacterRace} people. You must be a hard one to defeat with {HitPoints} hit points. ");
+            }
+            else if (CharacterLevel >= 50)
+            {
+                Console.WriteLine($"Welcome {CharacterName}. You are a growing young {CharClass}. The {CharacterRace} people must be looking forward to your development. Some day you'll be a hard one to defeat.");
+            }
+            else
+            {
+                if (InGuild == true)
+                {
+                    Console.WriteLine($"Look at you little {CharacterName}. Already in a programming guild. You'll go far.");
+                }
+                else
+                {
+                    Console.WriteLine($"You're in trouble {CharacterName}. It is hard to survive on your own out here. ");
+                }
+            }
+        }
+    }
+}
--- CSharpBasics/Program.cs
+++ CSharpBasics/Program.cs
@@ -8,15 +8,65 @@ namespace CSharpBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            MyString();
-
-            Console.ReadLine();
-        
+            MyCharacterClass();
         }
 
+        static void MyCharacterClass()
+        {
+            CharacterClass oldNinja = new CharacterClass();
+            CharacterClass mediumSamurai = new CharacterClass();
+            CharacterClass youngNinja = new CharacterClass();
+
+            oldNinja.CharacterName = "Ninjafiveo";
+            oldNinja.CharacterLevel = 105;
+            oldNinja.CharacterRace = "Human";
+            oldNinja.HitPoints = 5000;
+            oldNinja.CharClass = "Mage";
+            oldNinja.InGuild = true;
+            oldNinja.CharacterSelect();
+            Console.WriteLine();
+
+            mediumSamurai.CharacterName = "Bob";
+            mediumSamurai.CharacterLevel = 55;
+            mediumSamurai.CharacterRace = "Orc";
+            mediumSamurai.HitPoints = 5000;
+            mediumSamurai.CharClass = "Warrior";
+            mediumSamurai.InGuild = true;
+            mediumSamurai.CharacterSelect();
+            Console.WriteLine();
+
+            youngNinja.CharacterName = "Fred";
+            youngNinja.CharacterLevel = 30;
+            youngNinja.CharacterRace = "Orc";
+            youngNinja.HitPoints = 5000;
+            youngNinja.CharClass = "Warrior";
+            youngNinja.InGuild = true;
+            youngNinja.CharacterSelect();
+            Console.WriteLine();
+
+        }
+        static void myHoomans()
+        {
+            HoomanClass MrSekol = new HoomanClass();
+            MrSekol.age = 99;
+            MrSekol.dogName = "Mozzy";
+            MrSekol.hasDog = true;
+            MrSekol.name = "Michael";
+            MrSekol.Greeting();
+
+            HoomanClass Gaveeen = new HoomanClass();
+            Gaveeen.age = 23;
+            Gaveeen.hasDog = false;
+            Gaveeen.name = "NotRudiak";
+            Gaveeen.Greeting();
+
+
+
+        }
         static void MyString()
         {
+            // This is a comment.
+  
             Console.Write("What year were born?");
             int yearBorn = Convert.ToInt32(Console.ReadLine());
             Console.Write("What is your age?");
@@ -30,6 +80,89 @@ namespace CSharpBasics
             Console.WriteLine("This is my String. " + myString1);
             Console.WriteLine($"Here are two truths and a lie. {theTruth}, {theLie}");
             Console.WriteLine($"This is a change. {theTruth}, {theLie}");
+
+        }
+        static void DataTypes()
+        {
+            int myAge = 99;
+            string name = "Bob the Builder";
+            bool awesome = true;
+            char grade = 'F'; // Chars are single characters. requires single quotes. 'A'
+            float altitude = 90.265141f;  //float requires 'f' at the end.
+            double bankBalance = -4000000;
+            decimal pi = 3.14159265359M; // decimal requires an M
+
+            /// OutPut 
+            /// 
+            Console.WriteLine($"int = {myAge}.");
+            Console.WriteLine($"string = {name}.");
+            Console.WriteLine($"bool = {awesome}.");
+            Console.WriteLine($"char = {grade}.");
+            Console.WriteLine($"float = {altitude}.");
+            Console.WriteLine($"double = {bankBalance}.");
+            Console.WriteLine($"decimal = {pi}.");
+
+        }
+        static void InputTypes()
+        {
+            //DataTypes();
+            Console.WriteLine("Enter Number One");
+            //string myName = Console.ReadLine();
+            decimal numberOne = Convert.ToDecimal(Console.ReadLine());
+            //Console.WriteLine($"You entered: {numberOne}");
+
+            Console.WriteLine("Enter Number Two");
+            decimal numberTwo = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine($"You entered {numberOne} & {numberTwo}");
+
+            //Addition
+            Console.WriteLine($"Addition: {numberOne} + {numberTwo} = {numberOne + numberTwo}");
+            //Subtraction
+            Console.WriteLine($"Subtraction: {numberOne} - {numberTwo} = {numberOne - numberTwo}");
+            //Multiplication
+            Console.WriteLine($"Multiplication: {numberOne} * {numberTwo} = {numberOne * numberTwo}");
+            //Division
+            Console.WriteLine($"Division: {numberOne} / {numberTwo} = {numberOne / numberTwo}");
+            //Remainder (Modulus)
+            Console.WriteLine($"Modulus: {numberOne} % {numberTwo} = {numberOne % numberTwo}");
+
+            //Increment
+            Console.WriteLine($"Increment: The increment of {numberOne++} is {numberOne}");
+            Console.WriteLine($"Increment: The increment of {++numberOne} is {numberOne}");
+            Console.WriteLine($"numberOne++ is the same as numberOne = numberOne + 1. See here: {numberOne} is {numberOne = numberOne + 1} ");
+            //Decrement
+            Console.WriteLine($"Decrement: The deccrement of {numberTwo--} is {numberTwo}");
+            Console.WriteLine($"Decrement: The deccrement of {--numberTwo} is {numberTwo}");
+
+            decimal centeredPolygonal = ((((numberOne * numberTwo) / 2) * (numberTwo - 1)) + 1);
+
+            Console.WriteLine($"The centered polygonal \nof {numberOne} & {numberTwo} is: {centeredPolygonal}");
+
+            
+        }
+        static void CastingTypes()
+        {
+            double myDouble = 10.5;
+            int myInteger = 2;
+
+            myDouble = myDouble + myInteger;
+            Console.WriteLine($"Implicit cast: {myDouble}"); //Implicit Cast: C# Automatically Converts int to double
+
+            myDouble = (double)7 / myInteger;
+            Console.WriteLine($"Explicit cast: {myDouble}"); //Explicit Cast: C# If forced to Convert int to double
+                                                             //Without (double) answer is 3
+            myDouble = 7 / myInteger;
+            Console.WriteLine($"Without explicit cast: {myDouble}");
+
+            char myLetter = (char)65;
+            Console.WriteLine($"Cast integer: {myLetter}");
+
+
+            int myAscii = (int)'B';
+            Console.WriteLine($"Cast letter: {myAscii}");
+
+            Console.ReadKey();
 
         }
     }
