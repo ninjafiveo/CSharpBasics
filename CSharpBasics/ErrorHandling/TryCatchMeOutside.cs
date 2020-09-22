e5f86ef56c18f639dba394e79855ae633eec1a9a
Michael Sekol <mjsekol@live.com>
2020-09-22 11:32:16 -0400

    Catch - Create File if it does not exist.

--- CSharpBasics/ErrorHandling/TryCatchMeOutside.cs
+++ CSharpBasics/ErrorHandling/TryCatchMeOutside.cs
@@ -7,19 +7,43 @@ namespace CSharpBasics.ErrorHandling
 {
     class TryCatchMeOutside
     {
+        static public string documentPath = @"C:/Users/sekol_michael/source/repos/CSharpBasics/CSharpBasics/data.txt";
         public static void MyErrors()
         {
             try
             {
-                using (StreamReader sr = File.OpenText("bob.txt"))
+                using (StreamReader sr = File.OpenText(documentPath))
                 {
                     Console.WriteLine($"The first line of the text is: {sr.ReadLine()}");
                     Console.WriteLine($"The second line of the text is: {sr.ReadLine()}");
+
+                    sr.Close();
+
+                    StreamWriter sw = new StreamWriter(documentPath);
+                    Console.WriteLine("Give me a line to write.");
+                    string swInput = Console.ReadLine();
+                    sw.WriteLine(swInput);
+                    Console.WriteLine("Give me a second line to write.");
+                    sw.WriteLine(Console.ReadLine());
+                    Console.WriteLine("Check and see if it worked.");
+                    sw.Close();
+
                 }
             }
             catch (FileNotFoundException e)
             {
+
                 Console.WriteLine($"The file was not found: {e}");
+
+                if (!File.Exists(documentPath))
+                {
+                    using (StreamWriter sw = File.CreateText(documentPath));
+
+                }
+
+                MyErrors();
+
+
             }
             catch (DirectoryNotFoundException e)
             {
